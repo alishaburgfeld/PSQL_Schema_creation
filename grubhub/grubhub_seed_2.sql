298cec47b81b7aa72146ce7c55291e452d3f3a36
alishaburgfeld <alisha.burgfeld@gmail.com>
2022-06-29 21:22:39 -0400

    fixed those 2 errors

--- grubhub/grubhub_seed_2.sql
+++ grubhub/grubhub_seed_2.sql
@@ -1,4 +1,6 @@
+INSERT INTO users (id, username, password, email, address_id, first_name, last_name) VALUES (1, 'sing4thestarz', 'r0ckSt@r', 'rocknroll@hotmail.com', 2, 'Ashley', 'Mayberry');
 
+INSERT INTO orders (user_id, time_ordered, delivered, restaurant_id) VALUES (1, '2016-06-23 20:10:25-07', False, 2);
 
-
+INSERT INTO order_menu_items (order_id, menu_item_id, quantity) VALUES (2, 1, 1);
 
